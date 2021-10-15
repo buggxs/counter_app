6dbd199c9b84de71883bd10cb92ae5f7016d7603
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2021-10-15 08:34:36 +0200

    Add files via upload

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+import 'counter/view/counter_page.dart';
+
+class CounterApp extends MaterialApp {
+  const CounterApp({
+    Key key
+  }) : super(
+      key: key,
+      home: const CounterPage()
+  );
+}
